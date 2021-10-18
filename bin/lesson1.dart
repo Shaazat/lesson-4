878dd2a5b305f40b47b54b4dea11d9a40b9ff390
unknown <shaazat.nurlankyzy.kk@gamil.com>
2021-10-18 20:16:01 +0300

    add all files

---
+++ bin/lesson1.dart
@@ -0,0 +1,12 @@
+import 'dart:io'as go;
+
+void main() {
+  int x = 19;
+  var y = "sad";
+  //y ~/= x;
+  var name = "dsas";
+  //y = y + x;
+  if(y is! int && x  is int){
+    print("y is int");
+  }
+ // print(object)}
